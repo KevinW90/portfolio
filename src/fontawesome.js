b41e4f6e8f43c4bb8f062ae087e856f575a0e973
Kevin Williams <kevinjwilliams76@gmail.com>
2021-02-24 15:39:31 -0500

    fontawesome library

---
+++ src/fontawesome.js
@@ -0,0 +1,19 @@
+// import the library
+import {library} from '@fortawesome/fontawesome-svg-core';
+
+// import your icons
+import {
+  faUserAlt, 
+  faTools, 
+  faDraftingCompass, 
+  faStar, 
+  faCommentAlt
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faUserAlt, 
+  faTools, 
+  faDraftingCompass, 
+  faStar, 
+  faCommentAlt
+);
