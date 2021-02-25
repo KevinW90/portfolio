4c0fa9f5272481058b7d28b4d64b7573627ddecb
Kevin Williams <kevinjwilliams76@gmail.com>
2021-02-25 13:45:49 -0500

    -removed collapsed menu -added simple menu links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,40 +1,27 @@
 import '../styles/Nav.scss';
 
-import {useWindowWidth} from '../hooks';
-
 import LOGO from '../assets/LOGO.svg';
-import BARS from '../assets/bars.svg';
 
 function Nav() {
-  const windowWidth = useWindowWidth();
-
   return (
     <nav>
       <div>
         <img src={LOGO} alt="logo" />
       </div>
 
-      {windowWidth <= 600 ? <CollapsedMenu /> : <ExpandedMenu />}
+      <ul className="menu">
+        <li>
+          <a href='#'>home</a>
+        </li>
+        <li>
+          <a href='#'>projects</a>
+        </li>
+        <li>
+          <a href='#'>contact</a>
+        </li>
+      </ul>
     </nav>
-
   )
 }
 
-// collapsed menu component
-function CollapsedMenu() {
-  return (
-    <div className="menu-collapsed">
-      <img className="blah" src={BARS} alt="menu" />
-    </div>
-  )
-}
-
-// expanded menu component
-function ExpandedMenu() {
-  return (
-    <div>
-      expanded
-    </div>
-  )
-}
 export default Nav;
