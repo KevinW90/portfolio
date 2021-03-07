9f7d74cca7463fff2bd346f78045ab1cca65ce39
Kevin Williams <kevinjwilliams76@gmail.com>
2021-03-07 12:35:31 -0500

    fixed scrolling issue on re-render

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -3,9 +3,14 @@ import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
+  const rootElement = document.querySelector('#root');
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    rootElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
   }, [pathname]);
 
   return null;
